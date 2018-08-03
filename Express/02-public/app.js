d9539cf9a7de3d73f8c1f3e51b7a2d5b1ef46505
tom <76876201@qq.com>
2018-08-03 17:44:20 +0800

    18/8/3.txt

--- Express/02-public/app.js
+++ Express/02-public/app.js
@@ -3,10 +3,22 @@ const express = require('express');
 const app = express();
 //console.log(app);
 app.get('/',function(req,res){
-	res.send('<h1>hello 你好</h1>')
-})
+	res.send('get data ...')
+});
 
+app.post('/',function(req,res){
+	res.send('get data ...')
+});
 
+app.put('/',function(req,res){
+	res.send('get data ...')
+});
+
+app.delete('/',function(req,res){
+	res.send('get data ...')
+});
+
+app.use(express.static('public'));
 
 app.listen(3000,()=>{
 	console.log('Server running at http://127.0.0.1:3000');
